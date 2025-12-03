38bf9fa675e86fd7c01ff86c7f66ca468d03d242
Kshitij-112107026 <kamblekr21.extc@coeptech.ac.in>
2025-12-04 00:05:40 +0530

    done with lesson  6th and excercise as well for same

---
+++ ecommerce/src/pages/NotFoundPage.jsx
@@ -0,0 +1,12 @@
+import { Header } from "../components/header";
+
+export function NotFoundPage() {
+  return (
+    <>
+      <title>Page Not Found</title>
+      <Header />
+
+      <h2>Page does not exist</h2>
+    </>
+  );
+}
--- ecommerce/src/pages/Tracking.jsx
+++ ecommerce/src/pages/Tracking.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router";
 import { Header } from "../components/header";
 import "./tracking.css";
 export function Tracking() {
