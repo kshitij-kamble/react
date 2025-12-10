fc353d1a25c8a4100be0fb5e3d545c7a863ef04e
Kshitij-112107026 <kamblekr21.extc@coeptech.ac.in>
2025-12-10 23:59:33 +0530

    fetch implemented

--- ecommerce/src/pages/HomePage.jsx
+++ ecommerce/src/pages/HomePage.jsx
@@ -2,6 +2,13 @@ import { Header } from "../components/header";
 import "./HomePage.css";
 import { products } from "../../staring-code/data/products";
 export function HomePage() {
+  fetch("http://localhost:3000/api/products")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    });
   return (
     <>
       <title>Ecommerce Project</title>
